abebcd94db4eb5d15f4cd1556301a3d1c2e4b583
Shyam-Gi <shyamsundar.g.2019.ece@rajalakshmi.edu.in>
2021-09-26 13:03:34 +0530

    Use this main function for testing visuals and more

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from visualization import *
 sq = Square(50,30)
 
 initialShapeArray = sq.shapeMatrix
-
-ret_array = png2arr("./IMG/img.png")
-
+print(type(initialShapeArray))
+image = arr2png(initialShapeArray,name_="") 
+ret_array = png2arr('img.png')
+print(type(ret_array))
 print(len(ret_array),len(ret_array[0]))
 print(sq.shapeFrameDimension)
+if initialShapeArray==ret_array:
+    print(1)
+else:
+    print(0)
 
 
 
