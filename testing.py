03eeff09b680eccd4e0765d2ead5228aef6c192f
Sayad Pervez <38353201+SayadPervez@users.noreply.github.com>
2021-09-27 10:29:33 +0530

    Shyam's program to test various shapes

---
+++ testing.py
@@ -0,0 +1,15 @@
+from functions import *
+from shapes import Square
+import constants as cont
+from visualization import *
+
+sq = Square(50,30)
+
+initialShapeArray = sq.shapeMatrix
+print(type(initialShapeArray))
+image = arr2png(initialShapeArray,name_="") 
+ret_array = png2arr("./IMG/img.png")
+print(type(ret_array))
+print(len(ret_array),len(ret_array[0]))
+print(sq.shapeFrameDimension)
+print(initialShapeArray==ret_array)
