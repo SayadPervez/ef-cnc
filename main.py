da1d9d053c44dc728aa46d7aa6df3fe7a4c7fae4
Sayad Pervez <pervez2504@gmail.com>
2021-10-01 23:09:47 +0530

    shyam's rotate commit - size reduction bug yet to fix

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from shapes import Circle, Rectangle, Square
 import constants as cont
 import visualization as vis
 
-rect = Rectangle(20,6)
-#rect.displayShape()
-x = vis.rotate(rect.shapeMatrix,360)
+rect = Rectangle(21,1)
+x = vis.rotate(rect.shapeMatrix,0)
 print(len(x),len(x[0]))
 print(len(rect.shapeMatrix),len(rect.shapeMatrix[0]))
 
