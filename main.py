5b0effae546fe7f1f95439be4b550db435fc008a
Sayad Pervez <pervez2504@gmail.com>
2021-10-15 00:13:01 +0530

    All bugs resolved to this point !!!

--- main.py
+++ main.py
@@ -4,13 +4,3 @@ import constants as const
 import algorithm1,algorithm2
 from visualization import *
 from visualization import arr2png as a2p
-
-sq = Cone(10,2)
-print(sq.shapeFrameDimension)
-a2p(sq).show()
-
-input("Waiting for it ...")
-
-sq.tilt(15)
-print(sq.shapeFrameDimension)
-a2p(sq).show()
