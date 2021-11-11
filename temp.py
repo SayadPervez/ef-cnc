2920bbd33b7aa4afa392b10987d803ac6d096d17
Sayad Pervez <pervez2504@gmail.com>
2021-11-11 19:37:47 +0530

    COrrection commit

--- temp.py
+++ temp.py
@@ -1,11 +1,11 @@
+from cv2 import COLOR_BAYER_BG2BGR
 from shapes import *
 from visualization import *
 from functions import *
 import numpy as np
 
-sq = Square(6)
-sq.shapeMatrix = outline_with_shape(sq,3)
-sq.displayShape()
-input("")
-sq.shapeMatrix=rotate(sq,45)
-sq.displayShape()
+spe = Cone(20,12)
+
+x = rotate(evenize(spe.shapeMatrix),45)
+
+arr2png(x).show()
