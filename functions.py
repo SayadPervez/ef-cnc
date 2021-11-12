Angle updation and rotation bugs fixed

functions.py
@@ -167,12 +167,18 @@ def toggle2dArray(arr2d):
     '''
     return(np.array(arr2d).T.tolist())
 
-def npAnalyse(nparray):
-    print(c.Counter((nparray.reshape(-1)).tolist()))
+def npAnalyse(nparray,display=True):
+    if(display):
+        print(c.Counter((nparray.reshape(-1)).tolist()))
+    else:
+        return(c.Counter((nparray.reshape(-1)).tolist()))
 
-def liAnalyse(li):
+def liAnalyse(li,display=True):
     nparray = np.array(li)
-    print(c.Counter((nparray.reshape(-1)).tolist()))
+    if(display):
+        print(c.Counter((nparray.reshape(-1)).tolist()))
+    else:
+        return(c.Counter((nparray.reshape(-1)).tolist()))
 
 def outlineBugFixFunction(li2d):
     retli=[]

visualization.py
@@ -99,28 +99,53 @@ def rotate(obj,angle):
         if(obj.myShape=="CANVAS"):
             raise Exception("CANVAS cannot be rotated")
     a=np.array(arr,dtype=str)
-    a[a=='0.7']='r'
-    l,b=np.shape(a)
-    enlarge_factor = round((l**2+b**2)**0.5)*2
-    r=np.zeros(  (  enlarge_factor  ,  enlarge_factor  )             ,dtype=str)
-    r[(enlarge_factor//2)-(l//2):(enlarge_factor//2)+(l//2),(enlarge_factor//2)-(b//2):(enlarge_factor//2)+(b//2)]=a
-    r[r=='']='0'
-    r=arr2png(r,name_="")
-    r=r.rotate(angle)
-    r.save('./IMG/rotate.png')
-    r=png2arr('./IMG/rotate.png')
-    r=np.array(r,dtype=str)
-    res=np.where(r=='r')
-    top=min(res[1])
-    bottom=max(res[1])
-    right=max(res[0])
-    left=min(res[0])
-    rotated=r[left:right+1,top:bottom+1]
-    rotated[rotated=='r']=2
-    rotated = np.array(rotated,dtype=float)
-    rotated[rotated==2]=0.7
-    #npAnalyse(rotated)
-    return(rotated.tolist())
+    condAnalyse = npAnalyse(a,False)
+    cond = '0.7' in list(dict(condAnalyse).keys())
+    if(cond):
+        a[a=='0.7']='r'
+        l,b=np.shape(a)
+        enlarge_factor = round((l**2+b**2)**0.5)*2
+        r=np.zeros(  (  enlarge_factor  ,  enlarge_factor  )             ,dtype=str)
+        r[(enlarge_factor//2)-(l//2):(enlarge_factor//2)+(l//2),(enlarge_factor//2)-(b//2):(enlarge_factor//2)+(b//2)]=a
+        r[r=='']='0'
+        r=arr2png(r,name_="")
+        r=r.rotate(angle)
+        r.save('./IMG/rotate.png')
+        r=png2arr('./IMG/rotate.png')
+        r=np.array(r,dtype=str)
+        res=np.where(r=='r')
+        top=min(res[1])
+        bottom=max(res[1])
+        right=max(res[0])
+        left=min(res[0])
+        rotated=r[left:right+1,top:bottom+1]
+        rotated[rotated=='r']=2
+        rotated = np.array(rotated,dtype=float)
+        rotated[rotated==2]=0.7
+        #npAnalyse(rotated)
+        return(rotated.tolist())
+    else:
+        a[a=='1']='r'
+        l,b=np.shape(a)
+        enlarge_factor = round((l**2+b**2)**0.5)*2
+        r=np.zeros(  (  enlarge_factor  ,  enlarge_factor  )             ,dtype=str)
+        r[(enlarge_factor//2)-(l//2):(enlarge_factor//2)+(l//2),(enlarge_factor//2)-(b//2):(enlarge_factor//2)+(b//2)]=a
+        r[r=='']='0'
+        r=arr2png(r,name_="")
+        r=r.rotate(angle)
+        r.save('./IMG/rotate.png')
+        r=png2arr('./IMG/rotate.png')
+        r=np.array(r,dtype=str)
+        res=np.where(r=='r')
+        top=min(res[1])
+        bottom=max(res[1])
+        right=max(res[0])
+        left=min(res[0])
+        rotated=r[left:right,top:bottom]
+        rotated[rotated=='r']='1'
+        rotated = np.array(rotated,dtype=int)
+        #rotated=arr2png(rotated,name_="")
+        return(rotated.tolist())
 
 def color(shape,color):
     arr=np.array(shape , dtype=str)