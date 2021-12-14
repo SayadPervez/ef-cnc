976b614f84346cc9841ae35414dbb64c793447e7
Shyam-Gi <shyamsundar.g.2019.ece@rajalakshmi.edu.in>
2021-12-14 17:38:58 +0530

    Place svg

--- svg_shapes.py
+++ svg_shapes.py
@@ -1,6 +1,7 @@
 import os
 import cairo
 from numpy import pi, sqrt
+from numpy.core.arrayprint import _object_format
 
 def svgResize2Drawing(path):
     if("./" in path):
@@ -25,6 +26,8 @@ def svgRotate(path,angle,outputFileName = "new"):
     os.system(f'inkscape --batch-process --actions="select-all:all;transform-rotate:{str(angle)};FitCanvasToDrawing;export-filename:{path};export-do;" {path}')
 
 def createCanvas(width,height):
+    width=mm2pt(width)
+    height=mm2pt(height)
     with cairo.SVGSurface("canvas.svg", width,height) as surface:
         context = cairo.Context(surface)
         context.scale(700, 700)
@@ -32,6 +35,7 @@ def createCanvas(width,height):
 
 
 def Square(side,angle=0,name="Square"):
+    side=mm2pt(side)
     filename=name+".svg"
     with cairo.SVGSurface(filename,side*2 ,side*2) as surface:
         cr = cairo.Context(surface)
@@ -41,6 +45,8 @@ def Square(side,angle=0,name="Square"):
         svgResize2Drawing(filename)
 
 def Rectangle(l,b,angle=0,name="Rectangle"):
+    l=mm2pt(l)
+    b=mm2pt(b)
     filename=name+".svg"
     with cairo.SVGSurface(filename,sqrt(l**2 + b**2) , sqrt(l**2+b**2)) as surface:
         cr = cairo.Context(surface)
@@ -50,6 +56,7 @@ def Rectangle(l,b,angle=0,name="Rectangle"):
         svgResize2Drawing(filename) 
 
 def Circle(radius,name="Circle"):
+    radius=mm2pt(radius)
     filename=name+".svg"
     with cairo.SVGSurface(filename,2.5*radius , 2.5*radius) as surface:
         cr = cairo.Context(surface)
@@ -60,6 +67,8 @@ def Circle(radius,name="Circle"):
         svgResize2Drawing(filename) 
 
 def Cone(radius,height,angle=0,name="Cone"):
+    radius=mm2pt(radius)
+    height=mm2pt(height)
     filename=name+".svg"
     l=sqrt(radius**2 + height**2)
     theta1=0
@@ -73,8 +82,59 @@ def Cone(radius,height,angle=0,name="Cone"):
         svgRotate(filename,angle,outputFileName=name)
         svgResize2Drawing(filename)
 
-def svg_arrange(shape_list,canvas_size):
-    #Assuming shape_list = [[svg.Square(20),x1,y1],[svg.Circle(30),x2,y2],.....]
-    with cairo.SVGSurface("file.svg", canvas_size[0], canvas_size[1]) as surface:
-        cr = cairo.Context(surface)
+def mm2pt(x):
+    return x*2.83465
+
+def extract(file):
+    s=0; 
+    k=0; 
+    with open(file, "r") as file:
+        data = file.readlines()
+    for i,line in enumerate(data):
+        if(line.split()[0] =='<g'):
+            s=i
+            break
+    for j,line in enumerate(data):
+        if(line.split()[0]=='</g>'):
+            k=j
+    return data[s:k+1]
+
+def canvas_extract(file):
+    k=0; 
+    with open(file, "r") as file:
+        data = file.readlines()
+    for j,line in enumerate(data):
+        if(line.split()[0]=='</g>'):
+            k=j
+    return k
+
+def placeSVG(canvas,objectSVG,x,y):
+    x=mm2pt(x)
+    y=mm2pt(y)
+    if type(objectSVG)==type([]):
+        objectSVG=objectSVG
+    else:
+        objectSVG=[objectSVG]
+    for i,shapes in enumerate(objectSVG):
+        s=extract(shapes)
+        with open(canvas,"r") as a:
+            can=a.readlines()
+        k=canvas_extract(canvas)
+        s.insert(0,'<g id="id'+"-"+ str(i)+'"' + ' transform="translate('+str(x)+','+str(y)+')"\n' )
+        s.insert(len(s)-1,'</g>\n')
+        can[k:k]=s
+        #Creating new svg file
+        c=open('Canvas.svg','a') 
+        c.writelines(can)
+
+
+
+
+
+        
+
+
+    
+
+
 
